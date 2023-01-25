2dfa2a862e73d3a2d6228df9480d729c81781c27
natalia-giersa <n.giersa@gmail.com>
2023-01-25 17:28:36 +0100

    Transform Footer to StyledFooter

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,22 +2,22 @@ import CoffeeIcon from '@/assets/icons/coffee.svg';
 import HeartIcon from '@/assets/icons/heart.svg';
 import Typography from '@/components/Typography';
 
-import styles from './Footer.module.css';
+import * as Styles from './Footer.styles';
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>
-      <div className={`${styles['footer-box']} ${styles['footer-left-box']}`}>
+    <Styles.Footer>
+      <Styles.Box $side="left">
         <Typography text="Crafted with" color="gray" variant="small" />
         <CoffeeIcon />
         <Typography text="by Furu.DEV" color="gray" variant="small" />
-      </div>
-      <div className={`${styles['footer-box']} ${styles['footer-right-box']}`}>
+      </Styles.Box>
+      <Styles.Box $side="right">
         <Typography text="Coded with" color="gray" variant="small" />
         <HeartIcon />
         <Typography text="by N.Giersa" color="gray" variant="small" />
-      </div>
-    </footer>
+      </Styles.Box>
+    </Styles.Footer>
   );
 };
 

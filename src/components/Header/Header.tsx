4df5587de29fb52ae3d68d0b07e47fc7e94a37ed
natalia-giersa <n.giersa@gmail.com>
2023-01-26 10:28:14 +0100

    Transform Header to StyledHeader

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,19 @@
 import Button from '@/components/Button';
 import Logo from '@/components/Logo';
 
-import styles from './Header.module.css';
+import * as Styles from './Header.styles';
 import { HeaderProps } from './Header.types';
 
 const Header = ({ button }: HeaderProps) => {
   const { children, onClick } = button;
 
   return (
-    <div className={styles.header}>
+    <Styles.Header>
       <Logo />
       <Button onClick={onClick} variant="secondary">
         {children}
       </Button>
-    </div>
+    </Styles.Header>
   );
 };
 
